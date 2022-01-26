4ba55cbae06787ab725a9f6dcdd652ca6f781da2
Gorkem97 <gorkemucar3@hotmail.com>
2022-01-27 01:32:19 +0300

    Proto-Match_System
    Sürükleme ve tutma eylemleri kodlandı ve glitchlerden arındırıldı.

---
+++ Assets/Scripts/cube_removal.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cube_removal : MonoBehaviour
+{
+    public GameObject targetCube;
+    public Vector3 target_cube_position;
+    public Vector3 cube_start_position;
+    public bool icinde_mi;
+    private Camera mainCamera;
+    private float cameraZdistance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainCamera = Camera.main;
+        cameraZdistance =
+            mainCamera.WorldToScreenPoint(transform.position).z;
+        target_cube_position = targetCube.transform.position;
+        cube_start_position = transform.position;
+        icinde_mi = false;
+    }
+
+    void Update()
+    {
+
+    }
+    
+    private void OnMouseDrag()
+    {
+        Vector3 ScreenPosition =
+            new Vector3(Input.mousePosition.x,Input.mousePosition.y,cameraZdistance);
+        Vector3 WorldPosition =
+            mainCamera.ScreenToWorldPoint(ScreenPosition);
+        transform.position = WorldPosition;
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag== "target_cube")
+        {
+            icinde_mi = true;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        icinde_mi = false;
+    }
+    private void OnMouseUp()
+    {
+        if (icinde_mi==true)
+        {
+            transform.position = target_cube_position;
+        }
+        if (icinde_mi == false)
+        {
+            transform.position = cube_start_position;
+        }
+
+    }
+
+}
